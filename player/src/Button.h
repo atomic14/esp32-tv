9c1cb81833aa8d5270dbe391712b6c8c1ce42ab5
Hpsaturn <hpsaturn@gmail.com>
2023-10-07 18:21:08 +0200

    added touch driver and improved buttons for M5Core2

--- player/src/Button.h
+++ player/src/Button.h
@@ -2,26 +2,42 @@
 #ifdef M5CORE2
 #include "M5Touch.h"
 M5Touch Touch;
+TouchPoint_t pos;   
 #endif
+
+int _btn_left;
+int _btn_right;
 
 bool buttonRight(){
 #ifdef M5CORE2
-  TouchPoint_t pos   = Touch.getPressPoint();
-  if(pos.x>100) return true;
+  if (pos.x > 200 && pos.y > 90 && pos.y < 180) return true;
 #endif
 #ifdef BUTTON_R
-  return (digitalRead(BUTTON_R) == 0);
+  return (_btn_right == 0);
 #endif
   return false;
 }
 
 bool buttonLeft(){
 #ifdef M5CORE2
-  TouchPoint_t pos   = Touch.getPressPoint();
-  if(pos.x > 0 && pos.x<100) return true;
+  if (pos.x > 0 && pos.x < 100 && pos.y > 90 && pos.y < 180) return true;
 #endif
 #ifdef BUTTON_L
-  return (digitalRead(BUTTON_L) == 0);
+  return (_btn_left == 0);
+#endif
+  return false;
+}
+
+bool buttonUp(){
+#ifdef M5CORE2
+  if (pos.x > 106 && pos.x < 212 && pos.y > 0 && pos.y < 90) return true;
+#endif
+  return false;
+}
+
+bool buttonDown(){
+#ifdef M5CORE2
+  if (pos.x > 106 && pos.x < 212 && pos.y > 180 && pos.y < 280) return true;
 #endif
   return false;
 }
@@ -29,7 +45,7 @@ bool buttonLeft(){
 bool buttonPowerOff() {
 #ifdef BUTTON_L
   #ifdef BUTTON_R
-    return (digitalRead(BUTTON_L) == 0 && digitalRead(BUTTON_R) == 0);
+    return (_btn_left == 0 && _btn_right == 0);
   #endif
 #endif
   return false;
@@ -41,7 +57,27 @@ void buttonInit(){
   pinMode(BUTTON_L, INPUT_PULLUP);
   pinMode(BUTTON_R, INPUT);
   #endif
+#endif
+#ifdef M5CORE2
   Touch.begin();
 #endif
 }
+
+void buttonLoop(){
+  static uint_fast64_t buttonTimeStamp = 0;
+  if (millis() - buttonTimeStamp > 80) {
+    buttonTimeStamp = millis();
+    #ifdef M5CORE2
+    pos = Touch.getPressPoint();
+    // Serial.printf("(%i,%i)",pos.x,pos.y);
+    #endif
+    #ifdef BUTTON_L
+      #ifdef BUTTON_R
+    _btn_right = digitalRead(BUTTON_R);
+    _btn_left = digitalRead(BUTTON_L);
+      #endif
+    #endif
+  }
+}
+
 
